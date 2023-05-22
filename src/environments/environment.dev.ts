603de9337849100dc2aa62c120641e43a60cf318
cvchauhan <chirag.chauhan@prospecta.com>
2023-05-22 14:38:13 +0530

    add new script

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-//    baseUrl: 'http://saylussapidev.bancplus.in/api'
-   baseUrl: 'http://saylussapitest.bancplus.in/api',
+   baseUrl: 'http://saylussapidev.bancplus.in/api'
+   //baseUrl: 'http://saylussapitest.bancplus.in/api',
    //baseUrl: 'http://saylussapi.bancplus.in/api',
   };
   
